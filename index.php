4782fe858c8b19c9c9a564db64f83a8784dfea51
SofiaCurreri <sofiacurreri@gmail.com>
2023-03-09 18:42:23 +0100

    action form

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
 </head>
 <body>
     
-   <form method="GET" action="./censoring.php">
+   <form method="GET" action="censoring.php">
     <label for="paragraph"><h3>Scrivi un testo</h3></label>
     <textarea name="paragraph" id="" cols="30" rows="10"></textarea>
     <br>
